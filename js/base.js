1daa40f204c4ece9af7f6163854b9cc1ca1d64a1
Amanda Hagg <haggamanda@gmail.com>
2015-02-24 20:39:16 -0500

    first commit

---
+++ js/base.js
@@ -0,0 +1,6 @@
+console.log($("#wrapper"));
+console.log($("p"));
+// i don't think this worked -->
+console.log($(".input_block div"));
+// shouldn't it show each div across the class plus it says previous object and you said that was an issue. //
+console.log($("#message label"));
